f658614a8adaab04abfc307fca9a47c53eccb0a6
lexishanson <lexishanson@gmail.com>
2016-11-24 16:46:29 -0700

    Updated game.py file with execution

--- combat.py
+++ combat.py
@@ -4,11 +4,11 @@ import random
 class Combat:
   dodge_limit = 6
   attack_limit = 6
-  
+
   def dodge(self):
     roll = random.randint(1, self.dodge_limit)
     return roll > 4
-  
+
   def attack(self):
     roll = random.randint(1, self.attack_limit)
-    return roll > 4                       
+    return roll > 4
--- monsterv1.py
+++
@@ -1,9 +0,0 @@
-class Monster:
-    def __init__(self, **kwargs):
-        self.hit_points = hit_points
-        self.weapon = weapon
-        self.color = color
-        self.sound = sound
-
-    def battlecry(self):
-        return self.sound.upper()
